07842d1a38b96b397d2f5a479a0be9f589a42c0f
jinpeng <jinpeng.dong@gmail.com>
2018-11-22 17:12:33 +0800

    using antd: Layout and List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,51 @@
 import React, { Component } from 'react';
+import { Layout } from "antd";
+import { List, Avatar, Icon } from 'antd';
 import logo from './logo.svg';
 import './App.css';
+
+const {Header, Footer, Sider, Content} = Layout;
 
 function formatDate(date) {
     return date.toLocaleDateString();
 }
 
-function Avatar(props) {
-    return (
-        <img
-            className="Avatar"
-            src={props.user.avatarUrl}
-            alt={props.user.name}
-        />
-    );
-}
-
-function UserInfo(props) {
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div className="UserInfo-name">{props.user.name}</div>
-        </div>
-    );
-}
-
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <UserInfo user={props.comment.author} />
-            <div className="Comment-text">{props.comment.text}</div>
-            <div className="Comment-date">
-                {formatDate(props.comment.date)}
-            </div>
-        </div>
-    );
-}
+const IconText = ({ type, text }) => (
+    <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+        {text}
+  </span>
+);
 
 function CommentList(props) {
     const comments = props.comments;
-    const listItems = comments.map((comment) =>
-        <li key={comment.id}>
-            <Comment comment={comment}/>
-        </li>
-    );
     return (
-        <ul>{listItems}</ul>
+        <List
+            itemLayout="vertical"
+            size="large"
+            pagination={{
+                onChange: (page) => {
+                    console.log(page);
+                },
+                pageSize: 3,
+            }}
+            dataSource={comments}
+            footer={<div><b>ant design</b> footer part</div>}
+            renderItem={comment => (
+                <List.Item
+                    key={comment.id}
+                    actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
+                    extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+                >
+                    <List.Item.Meta
+                        avatar={<Avatar src={comment.author.avatarUrl} />}
+                        title={comment.author.name}
+                        description={formatDate(comment.date)}
+                    />
+                    {comment.content}
+                </List.Item>
+            )}
+        />
     );
 }
 
@@ -73,14 +73,17 @@ const comments = [
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-              <CommentList comments={comments}/>
-          </p>
-        </header>
-      </div>
+        <Layout>
+            <Header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </Header>
+            <Content>
+                <CommentList comments={comments}/>
+            </Content>
+            <Footer>
+                <div>Powered by GoBB</div>
+            </Footer>
+        </Layout>
     );
   }
 }
