c1ba138c8aedfec323ccb3b0223ddd2e0c837cd6
jinpeng <jinpeng.dong@gmail.com>
2018-11-22 13:43:16 +0800

    enable antd css

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -231,6 +231,7 @@ module.exports = {
                     },
                   },
                 ],
+                ['import', { libraryName: 'antd', style: 'css' }],
               ],
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
